44618b96b0ee3eb9fbe2062f34d97a4b76e948b2
Devit951 <devit951@gmail.com>
2019-07-04 13:43:28 +0500

    Added setOnSeekBarChangeListener

--- app/src/main/java/com/github/devit951/borderedimageview/MainActivity.kt
+++ app/src/main/java/com/github/devit951/borderedimageview/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.github.devit951.borderedimageview
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var borderWidth = 1f
+        var borderRadius = 1f
+
+        sb_border_radius.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                borderRadius = progress.toFloat()
+                iv_main.background = BorderDrawable(Color.RED, borderWidth, borderRadius)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        sb_border_width.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                borderWidth = progress.toFloat()
+                iv_main.background = BorderDrawable(Color.RED, borderWidth, borderRadius)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 }
